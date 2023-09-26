1db034806560405856f8220e6c33113ce8413bac
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-26 21:41:56 +0300

    Create 7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+#install Nginx web server (w/ Puppet)
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'first':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/EslamAlawamy permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
